b18437fd1edec391a0e65cfe4c6f7fe551abbb85
DuglasColoma <duglas.espanol@gmail.com>
2022-06-22 19:13:32 -0300

    working swagger

--- src/main/java/ar/edu/unq/desapp/grupoh/backenddesappapi/SwaggerConfig.java
+++ src/main/java/ar/edu/unq/desapp/grupoh/backenddesappapi/SwaggerConfig.java
@@ -11,7 +11,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
-
+/*
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
@@ -25,5 +25,34 @@ public class SwaggerConfig {
                 .build()
                 ;
     }
-    
+
+}*/
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket apiDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("ar.edu.unq.desapp.grupoh.backenddesappapi"))
+                //.apis(RequestHandlerSelectors.basePackage("ar.edu.unq.desapp.grupoh.backenddesappapi.webservice.controllers"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getApiInfo())
+                ;
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfo(
+                "CriptoP2P",
+                "CriptoP2P api",
+                "1.0",
+                "",
+                new Contact("Desapp-Grupo-H", "https://github.com/Desapp-Grupo-H/DesApp-Grupo-H-012022", ""),
+                "No License",
+                "",
+                Collections.emptyList()
+        );
+    }
 }
